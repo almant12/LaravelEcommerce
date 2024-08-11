590fffb03ff29a4ad935ab716cc5cae71e90a5db
almant12 <dokoalmant123@gmail.com>
2024-08-11 09:49:05 +0200

    push

--- resources/views/frontend/home/sections/flash-sale.blade.php
+++ resources/views/frontend/home/sections/flash-sale.blade.php
@@ -1,7 +1,6 @@
 @php
     $currentDate = \Carbon\Carbon::now();
     $flashDate = \Carbon\Carbon::parse(@$flashSaleDate->end_date);
-    @dd($currentDate,$flashDate);
 @endphp
 
 <section id="wsus__flash_sell" class="wsus__flash_sell_2">
