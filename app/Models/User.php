b85bfcca2f2d8ac32e85b6b9d13321d4e56e129f
almant12 <dokoalmant123@gmail.com>
2024-08-11 17:03:24 +0200

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
