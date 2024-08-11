09fab80822f4dc4fb5b735817b1328714a3cf9b9
almant12 <dokoalmant123@gmail.com>
2024-08-11 09:35:27 +0200

    push

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -28,8 +28,8 @@
                             <i class="fas fa-user-headset"></i>
                         </div>
                         <div class="wsus__call_text">
-                            <p>example@gmail.com</p>
-                            <p>+569875544220</p>
+                            <p>{{ $settings->contact_email}}</p>
+                            <p>{{ $settings->phone_number }}</p>
                         </div>
                     </div>
                     <ul class="wsus__icon_area">
