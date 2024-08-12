41203aa1e1147fe5b0156764effeb057c9642061
almant12 <dokoalmant123@gmail.com>
2024-08-12 13:30:17 +0200

    push

--- resources/views/frontend/home/sections/category-product-slide-one.blade.php
+++ resources/views/frontend/home/sections/category-product-slide-one.blade.php
@@ -1,8 +1,8 @@
 @php
 
 $productSectionOne = json_decode($categoryProductSectionOne->value);
-$firstCategory = Category::where('id',$productSectionOne->category);
-@dd($firstCategory);
+// $firstCategory = Category::where('id',$productSectionOne->category);
+@dd($productSectionOne->category);
 $lastKey = [];
 
 foreach ($productSectionOne as $key => $category){
