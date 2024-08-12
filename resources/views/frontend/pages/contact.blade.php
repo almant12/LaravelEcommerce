4e03cf265889cae61620e69f3788b777edfd42cc
almant12 <dokoalmant123@gmail.com>
2024-08-12 08:25:11 +0200

    push

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -95,7 +95,7 @@
                     <div class="col-xl-12">
                         <div class="wsus__con_map">
                             <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345093026!2d144.9630579153169!3d-37.81362797975171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf57725c9907f7f8!2sFederation+Square!5e0!3m2!1sen!2sau!4v1497321330767"
+  src="{{ $settings->map_url }}"
   width="600"
   height="450"
   style="border:0"
