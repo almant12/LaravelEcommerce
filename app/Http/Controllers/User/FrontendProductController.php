e490e906ac93b551660194fa1b4868c11671cf61
almant12 <dokoalmant123@gmail.com>
2024-08-12 13:08:01 +0200

    push

--- app/Http/Controllers/User/FrontendProductController.php
+++ app/Http/Controllers/User/FrontendProductController.php
@@ -18,7 +18,6 @@ class FrontendProductController extends Controller
 
     public function productIndex(Request $request){
         if ($request->has('category')){
-            dd($request->category);
             $category = Category::where('slug',$request->category)->firstOrFail();
             $products = Product::where(['category_id'=>$category->id,'status'=>1,'is_approved'=>1])
                 ->when($request->has('range'),function ($query) use ($request){
