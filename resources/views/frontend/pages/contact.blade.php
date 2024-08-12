8aa869afe7979262ffae948057bf5d62e3b075b1
almant12 <dokoalmant123@gmail.com>
2024-08-12 08:18:53 +0200

    push

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -14,7 +14,7 @@
                     <div class="col-12">
                         <h4>contact us</h4>
                         <ul>
-                            <li><a href="#">home</a></li>
+                            <li><a href="{{ route('home') }}">home</a></li>
                             <li><a href="#">contact us</a></li>
                         </ul>
                     </div>
@@ -94,8 +94,15 @@
                     </div>
                     <div class="col-xl-12">
                         <div class="wsus__con_map">
-                            <iframe width="100%" height="600" src="{{$settings->map_url}}"
-                                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                            <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345093026!2d144.9630579153169!3d-37.81362797975171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf57725c9907f7f8!2sFederation+Square!5e0!3m2!1sen!2sau!4v1497321330767"
+  width="600"
+  height="450"
+  style="border:0"
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade">
+</iframe>
                         </div>
                     </div>
                 </div>
