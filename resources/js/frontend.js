777c667b7be772f47146a0c8c291a8175b201755
almant12 <dokoalmant123@gmail.com>
2024-08-13 09:31:10 +0200

    cart

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -18,19 +18,29 @@ window.Echo.private('message.'+ USER.id).listen(
     "MessageEvent",
     (e) => {
         console.log(e);
-        let mainChatBox = $('.wsus__chat_area_body')
-        if (mainChatBox.attr('data-inbox') == e.sender_id){
-            var message = `<div class="wsus__chat_single">
-                <div class="wsus__chat_single_img">
-                    <img src="${e.sender_image}"
-                        alt="user" class="img-fluid">
-                </div>
-                <div class="wsus__chat_single_text">
-                    <p>${e.message.replace(/\n/g, '<br>')}</p>
-                     <span>${formatDateTime(e.date_time)}</span>
-                </div>
-            </div>`
-        }
+        let mainChatBox = $('.wsus__chat_area_body');
+const characterLimit = 100; // Set the character limit for each paragraph
+
+if (mainChatBox.attr('data-inbox') == e.sender_id) {
+    // Break down the message into chunks based on the character limit
+    let paragraphs = [];
+    for (let i = 0; i < e.message.length; i += characterLimit) {
+        paragraphs.push(e.message.substring(i, i + characterLimit));
+    }
+
+    // Join the paragraphs with <br> tags
+    let formattedMessage = paragraphs.join('<br><br>');
+
+    var message = `<div class="wsus__chat_single">
+        <div class="wsus__chat_single_img">
+            <img src="${e.sender_image}" alt="user" class="img-fluid">
+        </div>
+        <div class="wsus__chat_single_text">
+            <p>${formattedMessage}</p>
+            <span>${formatDateTime(e.date_time)}</span>
+        </div>
+    </div>`;
+}
         mainChatBox.append(message);
         scrollTobottom();
 
