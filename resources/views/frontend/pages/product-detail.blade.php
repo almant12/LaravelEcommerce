5eecd56bf86ce33f0d2fde57ad02b29153d7bce4
almant12 <dokoalmant123@gmail.com>
2024-08-13 08:12:42 +0200

    cart

--- resources/views/frontend/pages/product-detail.blade.php
+++ resources/views/frontend/pages/product-detail.blade.php
@@ -375,35 +375,6 @@
     <!--============================
         PRODUCT DETAILS END
     ==============================-->
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Send Message</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="" class="message_modal">
-                        @csrf
-                        <div class="form-group">
-                            <label for="">Message</label>
-                            <textarea name="message" class="form-control mt-2 message-box"></textarea>
-                            <input type="hidden" name="receiver_id" value="{{ $product->vendor->user_id }}">
-                        </div>
-
-                        <button type="submit" class="btn add_cart mt-4 send-button">Send</button>
-
-                    </form>
-
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-
     <!--============================
         RELATED PRODUCT START
     ==============================-->
